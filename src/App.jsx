2feea6fa2c57b1f7a95a5f587a0721c1a5960ba0
BenjaminJSLee <benjaminjslee@gmail.com>
2021-01-31 18:30:20 -0800

    Add icons for social media / github

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ function App() {
         name="Benjamin Lee"
         avatar="https://avatars.githubusercontent.com/u/49013733?s=460&u=fbf17ee4f03bd40c070f37520709f692fdaecba0&v=4"
         title="Junior Full Stack Developer"
+        github="https://github.com/BenjaminJSLee"
+        linkedin="https://www.linkedin.com/in/ben-lee-2a92871b1/"
+        email="benjaminjslee@gmail.com"
       />
     </div>
   );
