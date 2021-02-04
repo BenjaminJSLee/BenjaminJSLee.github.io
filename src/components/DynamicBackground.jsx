82a3aaddf577b9538ac3b8bd2dc1265941a8f359
BenjaminJSLee <benjaminjslee@gmail.com>
2021-02-04 14:18:33 -0800

    Add sun/moon cycle based on time of day

--- src/components/DynamicBackground.jsx
+++ src/components/DynamicBackground.jsx
@@ -23,12 +23,20 @@ const getCurrentTime = () => {
 };
 
 const DynamicBackground = (props) => {
-  const delay = `${getCurrentTime()}ms`;
+  const delay = getCurrentTime();
 
   return (
     <div className="background" style={props.style}>
-      <svg className="background-svg" style={{ animationDelay: `-${delay}` }}>
+      <svg className="background-svg" style={{ animationDelay: `-${delay}ms` }}>
       </svg>
+      <div className="satellites">
+        <svg className="sun" style={{ animationDelay: `-${Math.floor(delay + 64800000)}ms` }}>
+          <circle cx="50" cy="50" r="50"/>
+        </svg>
+        <svg className="moon" style={{ animationDelay: `-${Math.floor(delay + 21600000)}ms` }}>
+          <circle cx="50" cy="50" r="50"/>
+        </svg>
+      </div>
     </div>
   );
 };
